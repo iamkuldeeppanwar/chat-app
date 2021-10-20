2e36fd3d755776973a7f7ff750b30f801230632c
iamkuldeeppanwar <kuldeep10panwar@gmail.com>
2021-10-20 14:55:06 +0530

    seven commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const app = express();
 const server = http.createServer(app);
 // const socket = require("socket.io");
 // const io = socket(server);
+// const cors = require("cors");
 
+// app.use(cors());
 const io = require("socket.io")(server, {
   cors: {
     origin: "https://optimistic-colden-10b6c1.netlify.app",
@@ -27,11 +29,9 @@ io.on("connection", (socket) => {
     });
   });
 
-  socket.on(
-    "answerCall",
-    (data) => io.to(data.to).emit("callAccept"),
-    data.signal
-  );
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
 });
 
 // io.on("connection", (socket) => {
